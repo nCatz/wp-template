e379baee36909113ee023c8b339a62ffd25f9663
Yeray Ruiz <yeray.1997.yr@gmail.com>
2017-02-13 10:40:30 +0100

    Modified home (blog)

--- home.php
+++ home.php
@@ -4,44 +4,41 @@
 <?php if ( have_posts() ) : ?>
   <section>
     <div class="container">
-  <div id="grid-blog">
-    <?php while ( have_posts() ) : the_post(); ?>
-      
-      <div class="grid-blog-item">
-          <div class="card hoverable">
-              <?php if ( has_post_thumbnail() ) : ?>
-                <div class="card-image">
-                  <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-                      <?php the_post_thumbnail( 'full', array( 'class'  => 'blog-thumbnail-responsive' ) ); ?>
-                  </a>
-                  <a href="<?php the_permalink(); ?>"><span class="card-title <?php echo get_primary_text_color();?>-text"><?php the_title(); ?></span></a>
-                </div>
-              <?php else : ?>
+    <div class="row grid-blog">
+      <?php while ( have_posts() ) : the_post(); ?>
+        
+        <div class="col s12 m12 l6 grid-blog-item">
+            <div class="card hoverable grid-card-blog-item">
+                <?php if ( has_post_thumbnail() ) : ?>
+                  <div class="card-image">
+                    <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+                        <?php the_post_thumbnail( 'full', array( 'class'  => 'blog-thumbnail-responsive' ) ); ?>
+                    </a>
+                    <a href="<?php the_permalink(); ?>"><span class="card-title <?php echo get_primary_text_color();?>-text"><?php the_title(); ?></span></a>
+                  </div>
+                <?php else : ?>
+                  <div class="card-content">
+                    <a href="<?php the_permalink(); ?>"><span class="card-title <?php echo (get_primary_text_color() == "white")?"black":get_primary_text_color();?>-text"><?php the_title(); ?></span></a>
+                  </div>
+                  <div class="divider"></div>
+                <?php endif; ?>
                 <div class="card-content">
-                  <a href="<?php the_permalink(); ?>"><span class="card-title <?php echo (get_primary_text_color() == "white")?"black":get_primary_text_color();?>-text"><?php the_title(); ?></span></a>
-                    
+                  <?php the_excerpt(); ?>
                 </div>
-                <div class="divider"></div>
-              <?php endif; ?>
-              <div class="card-content">
-                <?php the_excerpt(); ?>
-              </div>
-              <div class="card-action">
-                <div class="row">
-                  <div class="col s6">Por <?php the_author_posts_link() ?></div>
-                  <div class="col s6"><time datatime="<?php the_time('Y-m-j'); ?>"><?php the_time('j F, Y'); ?></time></div>
+                <div class="card-action grid-card-action-blog-item">
+                  Por <?php the_author_posts_link() ?> <time datatime="<?php the_time('Y-m-j'); ?>"><?php the_time('j F, Y'); ?></time>
                 </div>
-              </div>
-          </div>
+            </div>
+        </div>
+      <?php endwhile; ?>
       </div>
-    <?php endwhile; ?>
     </div>
-    </div>
+    
     <div class="pagination">
       <span class="in-left"><?php next_posts_link('« Entradas antiguas'); ?></span>
       <span class="in-right"><?php previous_posts_link('Entradas más recientes »'); ?></span>
     </div>
-    <br><br><br><br><br>
+    </div>
   </section>
 <?php else : ?>
   <p><?php _e('Ups!, no hay entradas.'); ?></p>
