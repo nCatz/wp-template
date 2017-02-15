1c0b2e32234914e417deae32ae4b92cb432af0a6
Yeray Ruiz <yeray.1997.yr@gmail.com>
2017-02-15 17:30:55 +0100

    Fixed a css imported

--- inc/scripts.php
+++ inc/scripts.php
@@ -1,9 +1,9 @@
 <?php
 function load_scripts() {
     wp_enqueue_script( 'jquery', 'https://code.jquery.com/jquery-2.1.1.min.js' );
-    wp_enqueue_script( 'material', 'https://fonts.googleapis.com/icon?family=Material+Icons', array(), '1.0.0', true );
     wp_enqueue_script( 'script-materialize', get_template_directory_uri() . '/js/materialize.js', array(), '1.0.0', true );
     wp_enqueue_script( 'init', get_template_directory_uri() . '/js/init.js', array(), '1.0.0', true );
+    wp_enqueue_style( 'material', 'https://fonts.googleapis.com/icon?family=Material+Icons');
     wp_enqueue_style('materialize',get_template_directory_uri().'/css/materialize.css');
     wp_enqueue_style('style',get_template_directory_uri().'/style.css');
 }
