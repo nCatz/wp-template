87334b9f8696b80c41281ca3ab2c6a08b451df22
Yeray Ruiz <yeray.1997.yr@gmail.com>
2017-02-21 22:28:19 +0100

    Added 404.php page

---
+++ 404.php
@@ -0,0 +1,6 @@
+<?php get_header(); ?>
+<div class="container center">
+    <h1>ERROR 404 PAGE NOT FOUND </h1>
+    <h5>OnChuchesful() = false;</h5>
+</div> <!-- <div class="container"> -->
+<?php get_footer(); ?>
