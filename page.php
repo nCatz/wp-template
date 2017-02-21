bae0f11de7a89ddfa4ca58433371d2eb40fdf201
Yeray Ruiz <yeray.1997.yr@gmail.com>
2017-02-21 21:34:42 +0100

    Modified pages files

--- page.php
+++ page.php
@@ -3,6 +3,8 @@
     <div class="container row">
         <div class="col s12 card-panel">
             <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+                <h1><?php echo get_the_title(); ?></h1>
+                <div class="divider"></div>
                 <?php the_content(); ?>
                 <div class="divider"></div>
             <?php endwhile; endif; ?>
--- templates/no-container-template.php
+++ templates/no-container-template.php
@@ -4,5 +4,7 @@ Template Name: Template without card and container
 */
 ?>
 <?php get_header(); ?>
-
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+	<?php the_content(); ?>
+<?php endwhile; endif; ?>
 <?php get_footer(); ?>
